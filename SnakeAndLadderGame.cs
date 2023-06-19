a6fbd3dffe989eee288d264e74a7660944402350
Mahesh <Maheshk2104@gmail.com>
2023-06-19 11:52:04 +0530

    [Mahesh]Add: UC1 Start postion initialized to zero

---
+++ SnakeAndLadderGame.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderProblems
+{
+    public class SnakeAndLadderGame
+    {
+        public void Snake_and_Ladder()
+        {
+            Console.WriteLine("Snake and Ladder Game!");
+            int presentPosition = 0;
+            int diceRolls = 0;
+
+        }
+    }
+}
