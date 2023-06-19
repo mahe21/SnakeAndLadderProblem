24aa6013ef826ad396f8c2e53496a30064326c01
Mahesh <Maheshk2104@gmail.com>
2023-06-19 12:48:00 +0530

    [Mahesh]Add: UC4,5,6. Final single player game

--- SnakeAndLadderGame.cs
+++ SnakeAndLadderGame.cs
@@ -8,53 +8,56 @@ using System.Threading.Tasks;
 namespace SnakeAndLadderProblems
 {
     public class SnakeAndLadderGame
-    {
-        int presentPosition = 0;
-        int diceRolls = 0;
-        int diceValue;
-        int options;
-        Random rnd = new Random();
+    {        
         public void Snake_and_Ladder()
         {
             Console.WriteLine("Snake and Ladder Game!");
-            
-            
+            int presentPosition = 0;
+            int diceRolls = 0;
+
+            Random rnd = new Random();
 
             while (presentPosition < 100)
             {
-                
+                // to generate dice value from 1 to 6
                 int diceValue = rnd.Next(1,7);
                 diceRolls++;
                 Console.WriteLine("your dice value: " + diceValue);
-                DiceOptions();
-                
+                // To check if the player lands on ladder or snake.
 
+                int options = rnd.Next(0, 3);
+                switch (options)
+                {
+                    case 0:
+                        Console.WriteLine("No play!, You stay in the same position");
+                        break;
+                    case 1:
+                        presentPosition += diceValue;
+                        Console.WriteLine("Cool!, You landed on a ladder. You're going ahead by" + diceValue + "Position");
+                        break;
+                    case 2:
+                        presentPosition -= diceValue;
+                        if (presentPosition < 0)
+                        {
+                            presentPosition = 0;
+                        }
+                        Console.WriteLine("Sorry! You landed on a snake. You're going behind by " + diceValue + "Position");
+                        break;
+                }
+                // Move the player back if they exceed position 100
+                if (presentPosition > 100)
+                {
+                    presentPosition -= (presentPosition - 100); 
+                }
 
-                
+                Console.WriteLine("Your current position is " + presentPosition);
+                Console.WriteLine();
+            
             }
+            Console.WriteLine("Congratulations! You reached position 100.");
+            Console.WriteLine("Total dice rolls: " + diceRolls);
+            Console.ReadLine();
 
-        }
-        public void DiceOptions()
-        {
-            int options = rnd.Next(0, 3);
-            switch (options)
-            {
-                case 0:
-                    Console.WriteLine("No play!, You stay in the same position");
-                    break;
-                case 1:
-                    presentPosition = presentPosition + diceValue;
-                    Console.WriteLine("Cool!, You landed on a ladder. You're going ahead by" + diceValue + "Position");
-                    break;
-                case 2:
-                    presentPosition = presentPosition - diceValue;
-                    if (presentPosition < 0)
-                    {
-                        presentPosition = 0;
-                    }
-                    Console.WriteLine("Sorry! You landed on a snake. You're going behind by " + diceValue + "Position");
-                    break;
-            }
         }
     }
 }
